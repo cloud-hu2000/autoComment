378753258b0bb3bce6eb90c8a7e6dbefbd82c8aa
胡志勋 <huzx2006@fingard.com>
2026-05-13 09:05:52 +0800

    feat(batch): add per-item result statistics panel with multi-dimension filters

--- background.js
+++ background.js
@@ -1,10 +1,7 @@
 // 点击扩展图标时，在当前标签页内打开/关闭浮动窗口
 chrome.action.onClicked.addListener((tab) => {
-  if (tab && tab.id != null) {
-    chrome.tabs.sendMessage(tab.id, {
-      type: 'TOGGLE_PROMOTE_PANEL'
-    });
-  }
+  // 打开选项页面
+  chrome.tabs.create({ url: chrome.runtime.getURL('options.html') });
 });
 
 /**
